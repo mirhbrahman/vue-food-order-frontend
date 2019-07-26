572efdee2b3516e7451292a2dac051ae0820c5ea
MIR HB RAHMAN <ahadcr0@gmail.com>
2019-07-26 23:42:19 +0600

    Added category list at sidemenu

--- src/components/layouts/partials/SideMenu.vue
+++ src/components/layouts/partials/SideMenu.vue
@@ -1,53 +1,47 @@
 <template>
 	<div class="box">
-			<div class="side-search-wrap field has-addons">
-					<div class="control side-serch-input">
-						<input class="input" type="text" placeholder="Find food">
-					</div>
-					<div class="control">
-						<button type="submit" class="button is-primary">
-							<i class="fa fa-search"></i>
-						</button>
-					</div>
-				</div>
-				<hr>
+		<div class="side-search-wrap field has-addons">
+			<div class="control side-serch-input">
+				<input class="input" type="text" placeholder="Find food">
+			</div>
+			<div class="control">
+				<button type="submit" class="button is-primary">
+					<i class="fa fa-search"></i>
+				</button>
+			</div>
+		</div>
+		<hr>
 		<aside class="menu">
-		<p class="menu-label">
-			General
-		</p>
-		<ul class="menu-list">
-			<li><a>Dashboard</a></li>
-			<li><a>Customers</a></li>
-		</ul>
-		<p class="menu-label">
-			General
-		</p>
-		<ul class="menu-list">
-			<li><a>Dashboard</a></li>
-			<li><a>Customers</a></li>
-		</ul>
-		<p class="menu-label">
-			General
-		</p>
-		<ul class="menu-list">
-			<li><a>Dashboard</a></li>
-			<li><a>Customers</a></li>
-		</ul>
-		
-		<p class="menu-label">
-			Transactions
-		</p>
-		<ul class="menu-list">
-			<li><a>Payments</a></li>
-			<li><a>Transfers</a></li>
-			<li><a>Balance</a></li>
-		</ul>
-	</aside>
+			<div v-for="(cat, index) in categories" :key="index">
+				<a href="">
+					<p class="menu-label has-text-primary">
+					{{cat.name}}
+				</p>
+				</a>
+				<ul class="menu-list">
+					<li v-for="(subCat, index) in cat.sub_categories" :key="index">
+						<a>{{subCat.name}}</a>
+						<ul>
+							<li v-for="(childCat, index) in subCat.child_categories" :key="index">
+								<a>{{childCat.name}}</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+
+		</aside>
 	</div>
 </template>
 
 <script>
+	import store from '../../../store'
+	import { mapGetters } from 'vuex'
 	export default{
-
+		computed: {
+			...mapGetters({
+				categories: 'getCategories'
+			})
+		}
 	}
 </script>
