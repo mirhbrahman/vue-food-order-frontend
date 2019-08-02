50305fd4502203be8650cb35aead604a569dc7cb
MIR HB RAHMAN <ahadcr0@gmail.com>
2019-08-03 00:02:33 +0600

    Design payment card

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -47,13 +47,13 @@
 							<ul>
 								<li :class="{'is-active': address}">
 									<a @click="onAddressClick">
-										<span class="icon is-small"><i class="fas fa-image" aria-hidden="true"></i></span>
+										<span class="icon is-small"><i class="fas fa-address-card" aria-hidden="true"></i></span>
 										<span>Delivery address</span>
 									</a>
 								</li>
 								<li :class="{'is-active': payment}">
 									<a @click="onPaymentClick">
-										<span class="icon is-small"><i class="fas fa-music" aria-hidden="true"></i></span>
+										<span class="icon is-small"><i class="fas fa-money-check-alt" aria-hidden="true"></i></span>
 										<span>Payment</span>
 									</a>
 								</li>
@@ -62,60 +62,112 @@
 						
 						<div v-if="address">
 							<form>
-							<div class="field">
-								<label class="label">Recipient name</label>
-								<div class="control">
-									<input class="input" type="text">
+								<div class="field">
+									<label class="label">Recipient name</label>
+									<div class="control">
+										<input class="input" type="text">
+									</div>
 								</div>
-							</div>
-							<div class="field">
-								<label class="label">Phone</label>
-								<div class="control">
-									<input class="input" type="text">
+								<div class="field">
+									<label class="label">Phone</label>
+									<div class="control">
+										<input class="input" type="text">
+									</div>
 								</div>
-							</div>
-							<div class="field">
-								<label class="label">Address</label>
-								<div class="control">
-									<input class="input" type="text">
+								<div class="field">
+									<label class="label">Address</label>
+									<div class="control">
+										<input class="input" type="text">
+									</div>
 								</div>
-							</div>
-							<div class="field">
-								<label class="label">Address 2 (Optional)</label>
-								<div class="control">
-									<input class="input" type="text">
+								<div class="field">
+									<label class="label">Address 2 (Optional)</label>
+									<div class="control">
+										<input class="input" type="text">
+									</div>
 								</div>
-							</div>
 
-							<div class="columns">
-								<div class="column">
-									<div class="field">
-										<label class="label">State</label>
-										<div class="control">
-											<input class="input" type="text">
+								<div class="columns">
+									<div class="column">
+										<div class="field">
+											<label class="label">State</label>
+											<div class="control">
+												<input class="input" type="text">
+											</div>
+										</div>
+									</div>
+									<div class="column">
+										<div class="field">
+											<label class="label">Zip</label>
+											<div class="control">
+												<input class="input" type="text">
+											</div>
 										</div>
 									</div>
 								</div>
-								<div class="column">
-									<div class="field">
-										<label class="label">Zip</label>
-										<div class="control">
-											<input class="input" type="text">
-										</div>
-									</div>
-								</div>
-							</div>
-							
-						</form>
+
+							</form>
 						</div>
 
 						<div v-if="payment">
-							payment
+							<div class="control">
+								<label class="radio">
+									<input type="radio" name="answer" @click="onCODClick">
+									COD
+								</label>
+								<label class="radio">
+									<input type="radio" name="answer" @click="onCardClick">
+									Card
+								</label>
+							</div>
+
+							<div v-if="showCardPayment" class="columns mt-20">
+								<div class="column is-half">
+									<form>
+									<div class="columns">
+										<div class="column is-two-thirds">
+											<div class="field">
+												<label class="label">Card number</label>
+												<div class="control">
+													<input class="input" type="text" placeholder="">
+												</div>
+											</div>
+										</div>
+										<div class="column">
+											<div class="field">
+												<label class="label">Expires</label>
+												<div class="control">
+													<input class="input" type="text" placeholder="MM/YY">
+												</div>
+											</div>
+										</div>
+									</div>
+																		<div class="columns">
+										<div class="column is-two-thirds">
+											<div class="field">
+												<label class="label">Name on card</label>
+												<div class="control">
+													<input class="input" type="text" placeholder="">
+												</div>
+											</div>
+										</div>
+										<div class="column">
+											<div class="field">
+												<label class="label">Card code</label>
+												<div class="control">
+													<input class="input" type="text" placeholder="CVC">
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
+								</div>
+							</div>
 						</div>
 
 						<div class="columns">
 							<div class="column">
-								<router-link to="/checkout" class="button is-success checkout-btn">Checkout</router-link>
+								<router-link to="/checkout" class="button is-success checkout-btn" :disabled="paymentBtnInActive">Payment (${{grandTotal()}})</router-link>
 							</div>
 						</div>
 
@@ -156,7 +208,9 @@
 		data(){
 			return {
 				address: true,
-				payment: false
+				payment: false,
+				showCardPayment: false,
+				paymentBtnInActive: true
 			}
 		},
 		mounted(){
@@ -189,6 +243,12 @@
 			onPaymentClick(){
 				this.address = false
 				this.payment = true
+			},
+			onCODClick(){
+				
+			},
+			onCardClick(){
+				this.showCardPayment = true
 			}
 		}
 	}
